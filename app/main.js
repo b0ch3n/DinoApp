35c71290afd5f581e4409e8ff1925921de092ca4
b0ch3n <mbochen1@gmail.com>
2016-08-16 18:48:22 +0200

    m2

--- app/main.js
+++ app/main.js
@@ -1,9 +1,9 @@
 const electron = require('electron')
-// Child Process for keyword spotter
+
 const {spawn} = require('child_process')
-// Module to control application life.
+
 const app = electron.app
-// Module to create native browser window.
+
 const BrowserWindow = electron.BrowserWindow
 
 var mainWindow = null;
@@ -15,7 +15,7 @@ app.on('window-all-closed', function() {
 
 app.on('ready', function() { 
  mainWindow = new BrowserWindow({width: 800, height: 600}); 
- mainWindow.loadUrl('file://' + __dirname + '/index.html'); 
+ mainWindow.loadURL('file://' + __dirname + '/index.html'); 
  mainWindow.on('closed', function() {
  mainWindow = null;
  }); 
